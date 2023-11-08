f50de2ee2a08fa56b9b4b661fa321b906720b8b2
VictoriaGithendu <victoriagithendu@gmail.com>
2023-11-08 03:21:14 -0800

    python import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     x = len(sys.argv) - 1
     if x == 0:
-        print("{} arguments.".format(i))
+        print("{} arguments.".format(x))
     elif x == 1:
-        print("{} argument:".format(i))
+        print("{} argument:".format(x))
     else:
-        print("{} arguments:".format(i))
+        print("{} arguments:".format(x))
     if x >= 1:
         x = 0
         for arg in sys.argv:
