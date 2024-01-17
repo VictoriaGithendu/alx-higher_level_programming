0c1feba0fdd0d7e0d440e6247da275ad555244ef
VictoriaGithendu <victoriagithendu@gmail.com>
2024-01-17 02:21:57 -0800

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Creates a table known as 'first_table' in current database
+-- 'first_table' description (id INT) and (name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256);
+
