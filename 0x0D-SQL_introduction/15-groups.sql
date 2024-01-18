7ac1635a8d1d742f3e6139dcfb0d25f2a9c5ea50
VictoriaGithendu <victoriagithendu@gmail.com>
2024-01-17 22:41:15 -0800

    Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Lists records with the same score in the second_table
-SELECT score, COUNT('score') as number from second_table
+SELECT score, COUNT('score') as number FROM second_table
 GROUP BY score
 ORDER BY score DESC;
 
